9384dc586de327d2a427d82b6210cb07a87b43aa
Harsh Mangalam <hmangalam@oplglobal.com>
2021-10-02 08:45:29 +0530

    production

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: process.env.REACT_APP_ENDPOINT,
   cache: new InMemoryCache(),
 });
 
